37ed76adf6847e929cb805888e3d51f801d8efbb
William Walter <will.b.walter@gmail.com>
2015-06-09 16:15:27 -0400

    html sub links update

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$function () {
+	var menu       = $( ".nav_icon" );
+    var main_links = $( ".primary-nav-wrap > ul > li > a" );
+    var sub        = $( ".sub_nav ul a" );
+    var sub_nav    = $( ".sub-linka")
+    var wrapper    = $( ".wrapper" );
+    var back       = $(".backbutton");
+}
