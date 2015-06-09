9506ebf3b6735ff7e24ad0285cba8df274b270b9
William Walter <will.b.walter@gmail.com>
2015-06-09 17:23:40 -0400

    ugh, vanishing

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
 $function () {
 	var menu       = $( ".nav_icon" );
-    var main_links = $( ".primary-nav-wrap > ul > li > a" );
-    var sub        = $( ".sub_nav ul a" );
-    var sub_nav    = $( ".sub-linka")
+    var cat_links  = $( ".primary-nav-wrap > ul > li > a" );
+    var sub        = $( ".sub_nav" );
+    var sub_nav    = $( ".sub-links ul li a")
     var wrapper    = $( ".wrapper" );
-    var back       = $(".backbutton");
-}
+    var back       = $(".back");
+
+    .menu.on("click" function( e ) ){
+    	wrapper.addClass( "cat-nav");
+    	e.preventDefault();
+    )};
+	.cat_links.on("click" function( e ) {
+		wrapper.removeClass("cat-nav");
+		wrapper.addClass("sub-nav");
+		e.preventDefault();
+	)};
+	sub.on("click" function( e ) {
+		wrapper.removeClass("sub-nav");
+		wrapper.addClass("sub_links");
+		e.preventDefault();
+	});
+	sub_nav.on("click" function( e ) {
+		wrapper.removeClass("sub_sub_nav");
+		e.preventDefault();
+	});
+	back.on("click" function( e ) {
+		wrapper.removeClass( "cat_nav_state sub_nav_state" );
+        e.preventDefault();
+    });
+});
